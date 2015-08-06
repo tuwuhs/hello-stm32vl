1384279c78a8cdf6852bb75c1d0d5cae4d4edfdc
Tuwuh Sarwoprasojo <tuwuh@formulatrix.com>
2015-08-06 21:31:45 +0700

    Blinky in FreeRTOS

--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
-//
-// This file is part of the GNU ARM Eclipse distribution.
-// Copyright (c) 2014 Liviu Ionescu.
-//
-
-// ----------------------------------------------------------------------------
+/**
+ * main.c
+ *
+ * (c) 2015 Tuwuh Sarwoprasojo
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,28 +14,38 @@
 #include "queue.h"
 #include "timers.h"
 
-// ----------------------------------------------------------------------------
-//
-// Standalone STM32F1 empty sample (trace via NONE).
-//
-// Trace support is enabled by adding the TRACE macro definition.
-// By default the trace messages are forwarded to the NONE output,
-// but can be rerouted to any device or completely suppressed, by
-// changing the definitions required in system/src/diag/trace_impl.c
-// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
-//
+TimerHandle_t xLEDTimer1 = NULL;
+TimerHandle_t xLEDTimer2 = NULL;
 
-// ----- main() ---------------------------------------------------------------
+static void vLEDTimer1Callback(TimerHandle_t xTimer)
+{
+	static unsigned int ledState = 0;
 
-// Sample pragmas to cope with warnings. Please note the related line at
-// the end of this function, used to pop the compiler diagnostics status.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wmissing-declarations"
-#pragma GCC diagnostic ignored "-Wreturn-type"
+	if (ledState) {
+		GPIO_ResetBits(GPIOC, (1 << 8));
+	} else {
+		GPIO_SetBits(GPIOC, (1 << 8));
+	}
+	ledState ^= 1;
+}
+
+static void vLEDTimer2Callback(TimerHandle_t xTimer)
+{
+	static unsigned int ledState = 0;
+
+	if (ledState) {
+		GPIO_ResetBits(GPIOC, (1 << 9));
+	} else {
+		GPIO_SetBits(GPIOC, (1 << 9));
+	}
+	ledState ^= 1;
+}
 
 int main(int argc, char* argv[])
 {
+	// Ensure that all 4 interrupt priority bits are used as the pre-emption priority
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+
 	// Enable GPIO Peripheral clock
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
@@ -48,15 +57,18 @@ int main(int argc, char* argv[])
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-	// Infinite loop
-	while (1) {
-		GPIO_SetBits(GPIOC, (1 << 9));
-		for (volatile unsigned int i = 1000000; i; i--);
-		GPIO_ResetBits(GPIOC, (1 << 9));
-		for (volatile unsigned int i = 1000000; i; i--);
-	}
+	// Setup FreeRTOS
+	xLEDTimer1 = xTimerCreate("LEDTimer1", (300 / portTICK_PERIOD_MS), pdTRUE,
+			(void*) 0, vLEDTimer1Callback);
+	xTimerStart(xLEDTimer1, 0);
+
+	xLEDTimer2 = xTimerCreate("LEDTimer2", (370 / portTICK_PERIOD_MS), pdTRUE,
+			(void*) 0, vLEDTimer2Callback);
+	xTimerStart(xLEDTimer2, 0);
+
+	// Start FreeRTOS
+	vTaskStartScheduler();
+
+	// Infinite loop, should not reach here
+	while (1) {};
 }
-
-#pragma GCC diagnostic pop
-
-// ----------------------------------------------------------------------------
